4f6aa53b2eaaffa55360c32eb68259549d0687f4
Rodrigo Guinea <rodguinea@gmail.com>
2019-09-17 15:20:46 -0500

    Update install.R
    for parallel programming!

--- install.R
+++ install.R
@@ -1,4 +1,4 @@
 to_install_bio <- c("DESeq2", "WGCNA", "sva", "org.Hs.eg.db")
 BiocManager::install(to_install_bio)
-to_install <- c("dplyr","bestNormalize","glmnet","data.table","caret","MXM")
+to_install <- c("dplyr","bestNormalize","glmnet","data.table","caret","MXM", "foreach", "doParallel")
 install.packages(to_install)
